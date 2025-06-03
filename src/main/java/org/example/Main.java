c69335b7ad40ffdca9e920471e6251a3bd9758f8
Anselmo Ortega <lfkanselmo@gmail.com>
2025-06-03 09:56:31 -0500

    Se crean variables de prueba

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,50 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        
+        System.out.println("Reservas App");
+        System.out.println("=======================\n");
+
+        //Entradas del Algoritmo
+        Scanner entradateclado=new Scanner(System.in);
+        LocalDate fechaReserva;
+        LocalDate fechaMantenimiento;
+        String nombreHorario;
+        String reglamentoEspacio;
+        String correo;
+        String idUsuario;
+        String tipoUsuario;
+        String numeroApartamento;
+        String tipoApartamento;
+        String contraseñaUsuario;
+        String telefonoUsuario;
+
+        String horarioEspacio;
+        String nombreEspacio;
+        String reglamentoEspacios;
+        String utensiliosEspacio;
+
+        String codigoReserva;
+        String horarioReserva;
+
+        byte numeroBloque;
+        byte edadUsuario;
+        short cantidadAsistentes;
+        int valorReserva;
+
+        boolean esAdministrador=false;
+        boolean confirmacionReserva=false;
+        boolean estaOcupado=false;
+        boolean estaEnmantenimiento=false;
+
+
+
+
+
+
     }
 
 }
